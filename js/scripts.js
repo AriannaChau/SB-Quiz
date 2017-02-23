7c4d7c3fe2553d6898051bc1de6a65292ced830e
Arianna Chau and Steven Galvin <steven@>
2017-02-22 21:01:00 -0800

    add more js to add results together and determine which potential character to show.

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,27 @@
-
 $(document).ready(function() {
   $("form.quiz").submit(function(event) {
     event.preventDefault();
-    var answer1 = $('input:radio[name=question1]:checked').val();
-    console.log("Answer 1 is: " + answer1);
-    var answer2 = $('input:radio[name=question2]:checked').val();
-    console.log("Answer 2 is: " + answer2)
-    var answer3 = $('input:radio[name=question3]:checked').val();
-    console.log("Answer 3 is: " + answer3)
-    var answer4 = $('input:radio[name=question4]:checked').val();
-    console.log("Answer 4 is: " + answer4)
-    var answer5 = $('input:radio[name=question5]:checked').val();
-    console.log("Answer 5 is: " + answer5)
-});
+    var answer1 = parseInt($('input:radio[name=question1]:checked').val());
+    // console.log("Answer 1 is: " + answer1);
+    var answer2 = parseInt($('input:radio[name=question2]:checked').val());
+    // console.log("Answer 2 is: " + answer2)
+    var answer3 = parseInt($('input:radio[name=question3]:checked').val());
+    // console.log("Answer 3 is: " + answer3)
+    var answer4 = parseInt($('input:radio[name=question4]:checked').val());
+    // console.log("Answer 4 is: " + answer4)
+    var answer5 = parseInt($('input:radio[name=question5]:checked').val());
+    // console.log("Answer 5 is: " + answer5)
+    var userScore = (answer1 + answer2 + answer3 + answer4 + answer5)
+
+    $("#result").show();
+
+    if (userScore <= 7) {
+      $("#spongebob").show();
+
+    } else {
+      $("#squidward").show();
+    }
+  });
 
 
 
