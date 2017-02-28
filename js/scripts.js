28996d1321fddd6e76e6dae8abfc8631f26c2e3f
Arianna Chau and Steven Galvin <arianna@>
2017-02-27 20:53:03 -0800

    add looping to eliminate redundancies.

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,25 @@
 $(document).ready(function() {
   $("form.quiz").submit(function(event) {
     event.preventDefault();
-    var answer1 = parseInt($('input:radio[name=question1]:checked').val());
-    // console.log("Answer 1 is: " + answer1);
-    var answer2 = parseInt($('input:radio[name=question2]:checked').val());
-    // console.log("Answer 2 is: " + answer2)
-    var answer3 = parseInt($('input:radio[name=question3]:checked').val());
-    // console.log("Answer 3 is: " + answer3)
-    var answer4 = parseInt($('input:radio[name=question4]:checked').val());
-    // console.log("Answer 4 is: " + answer4)
-    var answer5 = parseInt($('input:radio[name=question5]:checked').val());
-    // console.log("Answer 5 is: " + answer5)
-    var userScore = (answer1 + answer2 + answer3 + answer4 + answer5)
+
+    var answers = ['question1', 'question2', 'question3', 'question4','question5'];
+    var total = 0
+    answers.forEach(function(answer) {
+      var userInput = parseInt($('input:radio[name=' + answer + ']:checked').val());
+      total += userInput;
+    });
+
+    // var answer1 = parseInt($('input:radio[name=question1]:checked').val());
+    // var answer2 = parseInt($('input:radio[name=question2]:checked').val());
+    // var answer3 = parseInt($('input:radio[name=question3]:checked').val());
+    // var answer4 = parseInt($('input:radio[name=question4]:checked').val());
+    // var answer5 = parseInt($('input:radio[name=question5]:checked').val());
+
+    console.log(total);
 
     $("#result").show();
 
-    if (userScore <= 7) {
+    if (total <= 7) {
       $("#spongebob").show();
 
     } else {
